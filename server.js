e5d9a70f371655274bb8df4a867081ede88efcde
Audrey Agyemang <audreyagyemang310@gmail.com>
2019-06-15 10:13:05 -0400

    saved.html into publis folder

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get("/scrape", function(req, res) {
 });
 
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
+app.get("/articles", function(req, res) {``
   // Grab every document in the Articles collection
   db.Article.find({})
     .then(function(dbArticle) {
